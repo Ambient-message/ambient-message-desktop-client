61a89785a75bb446aa0ed9379ccdda3016a0cf5c
yuzukianzu <yuzukianzu02@gmail.com>
2023-12-22 11:04:11 +0300

    feat: login page was added to project

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 postMessage({ payload: 'removeLoading' }, '*')
 
-// Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
   console.log(message)
 })
