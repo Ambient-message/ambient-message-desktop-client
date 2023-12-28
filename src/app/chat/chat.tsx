975deaa3de00e541bc271b5ad9e3b57dd200754b
yuzukianzu <yuzukianzu02@gmail.com>
2023-12-28 20:41:51 +0300

    feat: sidebar was added to chat

--- src/app/chat/chat.tsx
+++ src/app/chat/chat.tsx
@@ -1,39 +1,63 @@
+import { useState } from "react";
 import Button from "../../components/ui/button";
-import { Paperclip, SendHorizonal } from "lucide-react";
+import { Paperclip, SendHorizonal, Smile } from "lucide-react";
 
 export const Chat: React.FC = () => {
 
-    return (
-        <div className='flex-1 justify-between flex flex-col h-full'>
 
-            <div className='flex items-center justify-between py-3 border-b-2 border-gray-200'>
-                <div className='flex flex-col leading-tight ml-5'>
-                    <div className='flex items-center'>
-                        <span className='text-gray-700 mr-3 font-bold'>
-                            {"name"}
-                        </span>
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+
+    return (
+
+        <div className='flex-1 justify-between flex'>
+            <div className='flex-1 justify-between flex flex-col w-full h-full'>
+
+                <div className='flex items-center justify-between py-3 border-b-2 border-gray-200'>
+                    <div className='flex flex-col leading-tight ml-5'>
+                        <div className='flex items-center'>
+                            <span className='text-gray-700 mr-3 font-bold'>
+                                {"name"}
+                            </span>
+                        </div>
+                        <span className='text-sm text-gray-600'>{"last seen 1 hour ago"}</span>
                     </div>
-                    <span className='text-sm text-gray-600'>{"last seen 1 hour ago"}</span>
+                </div>
+
+                <div className='flex-1 overflow-y-auto'>
+                </div>
+
+                <div className='flex justify-stretch items-center p-3 border-t-2 border-gray-200'>
+                    <Button variant="ghost" className="w-15 h-15 ml-1 mr-2">
+                        <Paperclip className="w-5 h-5"></Paperclip>
+                    </Button>
+                    <input
+                        type='text'
+                        placeholder='Type your message...'
+                        className='flex-1 resize-y align-bottom h-11 px-5 rounded-lg bg-gray-200 border focus:outline-none'
+                    />
+                    <Button variant="ghost" 
+                    onClick={toggleSidebar}
+                    className="ml-2 w-15 h-15 ">
+                        <Smile className="w-5 h-5"></Smile>
+                    </Button>
+                    <Button variant="ghost"
+                        className="ml-2 w-15 h-15 ">
+                        <SendHorizonal className="w-5 h-5"></SendHorizonal>
+                    </Button>
                 </div>
             </div>
 
-            <div className='flex-1 overflow-y-auto'>
-            </div>
+            {isSidebarOpen && (
+                <div className='flex flex-col border-gray-200 border-r bg-white w-20 sidebar'>
 
-            <div className='flex justify-stretch items-center p-3 border-t-2 border-gray-200'>
-                <Button variant="ghost" className="w-15 h-15 ml-1 mr-2">
-                    <Paperclip className="w-5 h-5"></Paperclip>
-                </Button>
-                <input
-                    type='text'
-                    placeholder='Type your message...'
-                    className='flex-1 resize-y align-bottom h-11 px-5 rounded-lg bg-gray-200 border focus:outline-none'
-                />
-                <Button variant="ghost" className="ml-2 w-15 h-15 ">
-                    <SendHorizonal className="w-5 h-5"></SendHorizonal>
-                </Button>
-            </div>
-
+                </div>
+            )}
         </div>
+
     );
 };
--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -24,7 +24,7 @@ export const Home: React.FC = () => {
                             <tr>
                                 <td>
                                     <Button className='mt-0' variant="ghost">
-                                        <Settings className='w-6 h-6' />
+                                        <Settings className='w-5 h-5' />
                                     </Button>
                                 </td>
                             </tr>
@@ -33,9 +33,9 @@ export const Home: React.FC = () => {
                 </div>
             )}
 
-            <div className='hidden md:flex flex-grow h-full w-full max-w-xs flex-col gap-y-5 mr-auto overflow-y-auto border-r border-gray-200 bg-white px-6'>
+            <div className='hidden md:flex flex-grow h-full w-full max-w-xs flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white '>
 
-                <div className='flex items-center my-2'>
+                <div className='flex items-center my-2 ml-2'>
                     <Button
                         variant='ghost'
                         onClick={toggleSidebar}
@@ -50,73 +50,52 @@ export const Home: React.FC = () => {
                     />
                 </div>
 
-
-                <div className='text-xs font-semibold leading-5 text-gray-400'>
-                    Archived Chats
-                </div>
                 <div>
+                    <div className='text-xs font-semibold ml-5 mb-5 text-gray-400'>
+                        Your chats
+                    </div>
+
                     <nav className='flex flex-1 flex-col'>
-                        <ul role='list' className='flex flex-1 flex-col gap-y-7 hover:*:bg-gray-200'>
-                            <li className='cursor-pointer'>
-                                <div className='flex items-center gap-x-4 py-3'>
-                                    <div className='relative rounded-full h-10 w-10 bg-black'>
-                                    </div>
-                                    <div className='flex flex-col'>
-                                        <span aria-hidden='true'>Archived Chats</span>
-                                    </div>
-                                </div>
-                            </li>
+                        <ul role='list' className='flex flex-1 flex-col gap-y-7'>
+                            <nav className='flex flex-1 flex-col'>
+                                <ul role='list' className='flex flex-1 flex-col gap-y-7 hover:*:bg-gray-200'>
+                                    <li className='cursor-pointer'>
+                                        <div className='flex items-center gap-x-4 py-3 '>
+                                            <div className='relative rounded-full h-10 w-10 ml-5 bg-black'>
+                                            </div>
+                                            <div className='flex flex-col '>
+                                                <span aria-hidden='true'>Chat Name</span>
+                                                <span className='text-xs text-zinc-500' aria-hidden='true'>
+                                                    Last message
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </nav>
+
                         </ul>
                     </nav>
                 </div>
+                <nav className='mt-auto mb-3'>
+                    <li className='flex items'>
+                        <div className='flex flex-1 gap-x-4 text-sm font-semibold leading-6 text-gray-900'>
+                            <div className='relative rounded-3xl h-10 w-10 ml-5 bg-black'></div>
 
-                <div className='text-xs font-semibold leading-6 text-gray-400'>
-                    Your chats
-                </div>
+                            <span className='sr-only'>Your profile</span>
+                            <div className='flex flex-col'>
+                                <span aria-hidden='true'>Test</span>
+                                <span className='text-xs text-zinc-400' aria-hidden='true'>Test</span>
+                            </div>
+                        </div>
 
-                <nav className='flex flex-1 flex-col'>
-                    <ul role='list' className='flex flex-1 flex-col gap-y-7'>
-                        <nav className='flex flex-1 flex-col'>
-                            <ul role='list' className='flex flex-1 flex-col gap-y-7 hover:*:bg-gray-200'>
-                                <li className='cursor-pointer'>
-                                    <div className='flex items-center gap-x-4 py-3 '>
-                                        <div className='relative rounded-full h-10 w-10 bg-black'>
-                                        </div>
-                                        <div className='flex flex-col '>
-                                            <span aria-hidden='true'>Chat Name</span>
-                                            <span className='text-xs text-zinc-500' aria-hidden='true'>
-                                                Last message
-                                            </span>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </nav>
-
-                        <nav>
-                            <li className='-mx-6 mt-auto flex items-center my-1'>
-                                <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
-                                    <div className='relative rounded-3xl h-10 w-10 bg-black'>
-                                    </div>
-
-                                    <span className='sr-only'>Your profile</span>
-                                    <div className='flex flex-col'>
-                                        <span aria-hidden='true'>Test</span>
-                                        <span className='text-xs text-zinc-400' aria-hidden='true'>
-                                            Test
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <Button variant='ghost' className='h-15 w-15 mx-5 aspect-square'>
-                                    <LogOut className='w-4 h-4' onClick={() => {
-                                        localStorage.setItem("userInfo", "");
-                                        navigate('/')
-                                    }} />
-                                </Button>
-                            </li>
-                        </nav>
-                    </ul>
+                        <Button variant='ghost' className='h-15 w-15 mx-5 aspect-square'>
+                            <LogOut className='w-4 h-4' onClick={() => {
+                                localStorage.setItem("userInfo", "");
+                                navigate('/')
+                            }} />
+                        </Button>
+                    </li>
                 </nav>
             </div>
             <Chat />
